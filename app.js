4c527acff4b3c346bab7afc719d701b9410fc8f8
BLO2210 <brandonmichael2210@gmail.com>
2023-03-31 12:13:33 -0400

    Register groundwork

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const registerLoginRoutes = require("./routes/register-login");
 const chatroomRoutes = require("./routes/chatroom");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const bcrypt = require('bcryptjs')
+
+mongoose.connect('mongodb+srv://soundproofapp:<password>@soundproof.gsxwsfd.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+    console.log('DB connected')
+}).catch((error) => {
+    console.log(error)
+})
+
 
 app.use("/", registerLoginRoutes);
 
@@ -32,6 +41,30 @@ app.use(
     saveUninitialized: true,
   })
 );
+
+
+
+
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register-user', async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  let salt = await bcrypt.genSalt(10)
+  let hashedPassword = await bcrypt.hash(password, salt)
+
+  const user = new User({
+    username: username,
+    password: hashedPassword
+  })
+
+  await user.save()
+  res.redirect("/register");
+})
 
 http.listen(process.env.PORT, () => {
   console.log("Server is running...");
